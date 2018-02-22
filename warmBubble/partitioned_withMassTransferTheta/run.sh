b9381ab14296b2cd5b32b6513a1ee60cd86822e5
Hilary Weller <h.weller@reading.ac.uk>
2018-02-22 18:33:38 +0000

    Test cases warmBubble/partitioned_withMassTransferDiv and warmBubble/partitioned_withMassTransferTheta for partitionedExnerFoam/advectiveForm which is now working stably having corrected some bugs. Mass transfer options based on horizontal divergence and based on laplacian(theta)

---
+++ warmBubble/partitioned_withMassTransferTheta/run.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -e
+
+# clear out old stuff
+rm -rf [0-9]* constant/polyMesh core log
+
+# create mesh
+blockMesh
+
+# hydrostatically balanced initial conditions
+rm -rf [0-9]* core
+mkdir 0
+cp -r init_0/* 0
+setExnerBalancedH
+
+# change Exner BC from fixedValue to hydroStaticExner
+sed -i 's/fixedFluxBuoyantExner/partitionedHydrostaticExner/g' 0/Exner
+
+# Add a warm perturnation
+cp 0/theta 0/theta_init
+#cp constant/initialProperties0 constant/initialProperties
+#makeHotBubble
+#mv 0/theta 0/stable.theta
+#cp constant/initialProperties1 constant/initialProperties
+makeHotBubble
+mv 0/theta 0/stable.theta
+cp 0/theta_init 0/buoyant.theta
+
+# Partition into stable and buoyant fluids
+#cp 0/buoyant.theta 0/stable.theta
+#mv 0/theta_init 0/buoyant.theta
+mv 0/Uf 0/stable.Uf
+cp 0/stable.Uf 0/buoyant.Uf
+rm 0/thetaf
+
+# create initial conditions
+#setFields
+#sumFields 0 stable.sigma init_0 stable.sigma 0 buoyant.sigma -scale1 -1
+
+# Plot initial conditions
+gmtFoam theta -time 0
+gv 0/theta.pdf &
+gmtFoam sigma -time 0
+gv 0/sigma.pdf &
+
+# Solve Euler equations
+#partitionedExnerFoam >& log & sleep 0.01; tail -f log
+partitionedExnerFoamAdv >& log & sleep 0.01; tail -f log
+
+# Plot theta and sigma
+time=100
+for var in theta sigma; do
+   gmtFoam $var -time $time
+   gv $time/$var.pdf &
+done
+
+# animate the results
+for field in theta sigma; do
+    gmtFoam $field
+    eps2gif $field.gif 0/$field.pdf ??/$field.pdf ???/$field.pdf ????/$field.pdf
+done
+
+# Make links for animategraphics
+mkdir -p animategraphics
+for field in theta sigma; do
+    t=0
+    for time in [0-9] [0-9]?? [0-9]???; do
+        ln -s ../$time/$field.pdf animategraphics/${field}_$t.pdf
+        let t=$t+1
+    done
+done
+

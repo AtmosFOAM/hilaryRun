5a2eacc58ac8d025f431f0c8a68630ded1055f51
Hilary Weller <h.weller@reading.ac.uk>
2018-06-11 17:56:51 +0100

    Added diagnostics for exnerFoamTurbulence for case GABLS_1d_SBL/exnerFoamTurbulence

---
+++ GABLS_1d_SBL/bouyantPimpleFoam/runAll.sh
@@ -0,0 +1,33 @@
+# Set up, run and plot the results from the Cuxart et al boundary layer case
+# with buoyantPimpleFoam
+
+blockMesh
+rm -rf [0-9]* core log* *dat
+cp -r ../skeleton/init_0 0
+
+# Initial theta
+setTheta
+rm 0/thetaf 0/BruntFreq2f
+
+# Initial k
+setAnalyticTracerField -name k -tracerDict k_tracerFieldDict
+
+# Setup hydrostatic pressure
+setHydroStaticPressure
+# Fix the top boundary conditions
+sed -i 's/fixedValue/hydrostatic_p_rgh;\n        gradient uniform 0/g' 0/p_rgh
+
+# run buoyantPimpleFoam
+rm -rf [1-9]*
+buoyantPimpleFoam >& log & sleep 0.01; tail -f log
+
+# Plotting for buoyantPimpleFoam
+for var in  theta T p p_rgh U k nut epsilon; do
+    writeCellDataxyz $var
+    for time in [0-9]*; do
+        sort -g --key=3 $time/$var.xyz > $time/$var.dat
+    done
+    gmtPlot ../plots/plotAll$var.gmt
+    rm */$var.dat */$var.xyz
+done
+

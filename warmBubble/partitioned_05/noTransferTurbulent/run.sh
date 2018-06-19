71275f682b30d7ab6532446f54d414956ad24441
Hilary Weller <h.weller@reading.ac.uk>
2018-06-19 10:34:30 +0100

    Getting case warmBubble/partitioned_05/noTransferTurbulent with code partitionedTurbulentFoam

--- warmBubble/partitioned_05/noTransferTurbulent/run.sh
+++ warmBubble/partitioned_05/noTransferTurbulent/run.sh
@@ -22,15 +22,12 @@ makeHotBubble
 # Warm bubble only in the buoyant partition
 mv 0/theta 0/theta.buoyant
 cp init_0/theta 0/theta.stable
-mv 0/Uf 0/Uf.stable
-cp 0/Uf.stable 0/Uf.buoyant
-mv 0/u 0/u.stable
-cp 0/u.stable 0/u.buoyant
+for var in Uf u alphat nut k epsilon; do
+    cp init_0/$var 0/$var.buoyant
+    cp init_0/$var 0/$var.stable
+    rm -f 0/$var
+done
 rm 0/thetaf
-mv 0/alphat 0/alphat.stable; cp 0/alphat.stable 0/alphat.buoyant
-mv 0/nut 0/nut.stable; cp 0/nut.stable 0/nut.buoyant
-mv 0/k 0/k.stable; cp 0/k.stable 0/k.buoyant
-mv 0/epsilon 0/epsilon.stable; cp 0/epsilon.stable 0/epsilon.buoyant
 
 # create initial sigma field
 setFields
@@ -48,7 +45,7 @@ gmtPlot ../../plots/plotCo.gmt
 gmtPlot ../../plots/plotEnergy.gmt
 
 # Debugging diagnostics
-for var in k epsilon; do
+for var in nut k epsilon sigmaTheta; do
     gmtFoam -time $time $var
     gv $time/$var.pdf &
 done
---
+++ warmBubble/turbulent/adv/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+# clear out old stuff
+rm -rf [0-9]* constant/polyMesh core log
+
+# create mesh
+blockMesh
+
+# hydrostatically balanced initial conditions
+rm -rf [0-9]* core
+mkdir 0
+cp -r init_0/* 0
+setExnerBalancedH
+
+# Add a warm perturnation
+cp 0/theta constant/theta_init
+cp 0/theta 0/theta_init
+makeHotBubble
+rm 0/thetaf
+
+# Plot initial theta
+time=0
+gmtFoam theta -time $time
+gv $time/theta.pdf &
+
+# Solve Euler equations
+exnerFoamTurbulenceAdv >& log & sleep 0.01; tail -f log
+
+# Debugging diagnostics
+time=100
+for var in nut k epsilon theta; do
+    gmtFoam -time $time $var
+    gv $time/$var.pdf &
+done
+

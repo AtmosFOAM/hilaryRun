51479e1e606863c60907b2f0a205a379018e27d7
Hilary Weller <h.weller@reading.ac.uk>
2019-02-25 17:15:47 +0000

    Test case run/hilary/warmBubble/Boussinesq/singleColumn for code AtmosFOAM-multiFluid/applications/solvers/Boussinesq/multiFluidBoussinesqFoam The total velocity appears not to be divergence free

---
+++ warmBubble/Boussinesq/singleColumn/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# clear out old stuff
+rm -rf [0-9]* constant/polyMesh core log
+
+# create mesh
+blockMesh
+
+# Stationary initial conditions
+rm -rf [0-9]* core
+mkdir 0
+cp -r init_0/* 0
+
+# Create non-uniform sigma and Stransfer fields
+setFields -dict system/setS01Dict
+setFields -dict system/setS10Dict
+setFields -dict system/setSigmaDict
+sumFields 0 sigma.stable init_0 sigma.stable 0 sigma.buoyant -scale1 -1
+
+# Solve multi-fluid Boussinesq equations
+multiFluidBoussinesqFoam >& log & sleep 0.01; tail -f log
+
+gmtFoam sigmab
+eps2gif sigmab.gif 0/sigmab.pdf ??/sigmab.pdf ???/sigmab.pdf \
+        ????/sigmab.pdf
+

29d06d0d89b191bfb54235aa22e07fb2650f6c27
Hilary Weller <h.weller@reading.ac.uk>
2019-03-27 16:14:59 +0000

    Fixed flow over boundary conditions for multiFluidBoussinesqFoam when different buoyancies are used at the boundaries. Test case in RCE/Boussinesq/singleColumn_2fluid

--- RCE/Boussinesq/mediumRes/run.sh
+++ RCE/Boussinesq/mediumRes/run.sh
@@ -17,21 +17,14 @@ setFields
 # Solve Euler equations
 multiFluidBoussinesqFoam >& log & sleep 0.01; tail -f log
 
-# Plot all fields
-for time in [0-9]*; do
-    for field in sigma b b.stable b.buoyant S01 S10; do
-        gmtFoam -time $time $field
-    done
-    sed 's/TIMEDIR/'$time'/g' plots/plotFields.tex > plotFields.tex; \
-        pdflatex plotFields.tex; \
-        pdfcrop plotFields.pdf $time/plotFields.pdf; \
-        rm plotFields.*
-done
-eps2gif allFields.gif 0/plotFields.pdf ?/plotFields.pdf ??/plotFields.pdf \
-        ???/plotFields.pdf
+# Plots
+writeuvw u
 
-gmtFoam b
-eps2gif b.gif 0/b.pdf ????/b.pdf ?????/b.pdf ??????/b.pdf
+for var in b w; do
+    gmtFoam $var
+    eps2gif $var.gif 0/$var.pdf ????/$var.pdf ?????/$var.pdf ??????/$var.pdf
+done
+
 
 # Plot cooling rate, Q
 grep value */Q | awk -F'/' '{print $1, $2}' | awk '{print $1, $3}' \

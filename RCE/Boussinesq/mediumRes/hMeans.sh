fd45f14e0bd5a5aa35e5fe9244a5680ca2d91970
Hilary Weller <h.weller@reading.ac.uk>
2019-04-25 17:01:13 +0100

    Modifying test case RCE/Boussinesq/singleColumn_2fluid to maintain a steady state

--- RCE/Boussinesq/mediumRes/hMeans.sh
+++ RCE/Boussinesq/mediumRes/hMeans.sh
@@ -3,31 +3,41 @@
 # Calculate horizontal means conditioned on vertical velocity and plot
 
 time=200000
-# Create cell sets "rising" and "falling" dependent on w
+## Create cell sets "rising" and "falling" dependent on w
 writeuvw u -time $time
-topoSet -dict system/conditionalSamplingDict -time $time
+#topoSet -dict system/conditionalSamplingDict -time $time
 
-# Conditioanl horizontal means of b and w
-horizontalMean -time $time -cellSet rising
-horizontalMean -time $time -cellSet falling
-horizontalMean -time $time
+## Conditional horizontal means of b and w
+#horizontalMean -time $time -cellSet rising
+#horizontalMean -time $time -cellSet falling
+#horizontalMean -time $time
 
-# Write out mean plus/minus one standard deviation of b and w
-for var in b uz P; do for set in rising falling; do
-    awk '{print $1, $2, $3, $4, $4-$5, $4+$5, $6, $7}' \
-        $time/horizontalMean_${set}_${var}.dat \
-        | sponge $time/horizontalMean_${set}_${var}.dat
-done; done
+## Write out mean plus/minus one standard deviation of b and w
+#for var in b uz P; do for set in rising falling; do
+#    awk '{print $1, $2, $3, $4, $4-$5, $4+$5, $6, $7}' \
+#        $time/horizontalMean_${set}_${var}.dat \
+#        | sponge $time/horizontalMean_${set}_${var}.dat
+#done; done
 
-# plots
-for var in b w sigma P; do
-    sed 's/TIME/'$time'/g' plots/$var.gmt > plots/tmp.gmt
-    gmtPlot plots/tmp.gmt
-done
-rm plots/tmp.gmt
+## plots
+#for var in b w sigma P; do
+#    sed 's/TIME/'$time'/g' plots/$var.gmt > plots/tmp.gmt
+#    gmtPlot plots/tmp.gmt
+#done
+#rm plots/tmp.gmt
 
 # Redefine sigma based on w
 setFields -dict system/conditionalSamplingDict -noFunctionObjects
 
-# Rerun for one time step and average onto a coarse grid
+# Horizontal mean again
+horizontalMean -time $time
+
+# Redifine sigma at time 200020
 multiFluidBoussinesqFoam
+setFields -dict system/conditionalSamplingDict -noFunctionObjects
+
+# Horizontal mean again
+time=200020
+writeuvw u -time $time
+horizontalMean -time $time
+
--- RCE/Boussinesq/singleColumn_2fluid/run.sh
+++ RCE/Boussinesq/singleColumn_2fluid/run.sh
@@ -6,18 +6,45 @@ rm -rf [0-9]* constant/polyMesh core log
 # create mesh
 blockMesh
 
-# Initial conditions
+# Initial conditions from ../mediumRes/
 rm -rf [0-9]* core
 mkdir 0
 cp -r init_0/* 0
 
-# Initial buoyancy perturbations and transfer at the ground
-setFields
-cp 0/transferLocation constant
+FROM=../mediumRes/200000
 
-# Solve Euler equations
+values=`awk '{if(NR>1)print $4}' $FROM/horizontalMean_none_sigma.stable.dat | paste -s`
+sed -i 's/REPLACE/'"$values"'/g' 0/sigma.stable
+
+values=`awk '{if(NR>1)print $4}' $FROM/horizontalMean_none_sigma.buoyant.dat | paste -s`
+sed -i 's/REPLACE/'"$values"'/g' 0/sigma.buoyant
+
+values=`awk '{if(NR>1)print $4}' $FROM/horizontalMean_sigma.stable_b.dat | paste -s`
+sed -i 's/REPLACE/'"$values"'/g' 0/b.stable
+
+values=`awk '{if(NR>1)print $4}' $FROM/horizontalMean_sigma.buoyant_b.dat | paste -s`
+sed -i 's/REPLACE/'"$values"'/g' 0/b.buoyant
+
+values=`awk '{if(NR>1)print "("0, 0, $4")"}' $FROM/horizontalMean_sigma.stable_uz.dat | paste -s`
+sed -i 's/REPLACE/'"$values"'/g' 0/u.stable
+
+values=`awk '{if(NR>1)print "("0, 0, $4")"}' $FROM/horizontalMean_sigma.buoyant_uz.dat | paste -s`
+sed -i 's/REPLACE/'"$values"'/g' 0/u.buoyant
+
+# Initial buoyancy perturbations and transfer at the ground
+#setFields
+
+# Transfers everywhere to keep sigma uniform
+mv 0/transferLocation constant
+
+# Solve Boussinesq equations
 rm -rf [1-9]* core
 multiFluidBoussinesqFoam >& log & sleep 0.01; tail -f log
+
+# Plot cooling rate, Q
+grep value [0-9]*/Q | awk -F'/' '{print $1, $2}' | awk '{print $1, $3}' \
+     | awk -F';' '{print $1}'| sort -n > plots/Q.dat
+gmtPlot plots/Q.gmt
 
 # Create ascii data files of all fields
 for field in sigma.buoyant b b.stable b.buoyant Uf Uf.buoyant Uf.stable \

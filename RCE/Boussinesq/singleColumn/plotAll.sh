07324901ff324f7bc1f56e109e2672472d0fd86f
Hilary Weller <h.weller@reading.ac.uk>
2019-09-19 04:47:56 +0100

    Updating test cases RCE/Boussinesq and warmBubble/Boussinesq for multifluid Boussinesq code

---
+++ RCE/Boussinesq/singleColumn/plotAll.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# Graphs for all cases
+time=100000
+for dir in *Transfer*; do
+    cd $dir
+    # Create ascii data files of all fields
+    for field in sigma.buoyant b b.stable b.buoyant Uf Uf.buoyant Uf.stable \
+             massTransfer.buoyant.stable massTransfer.stable.buoyant dPdz.stable \
+             dPdz.buoyant ; do
+        for t in 0 $time; do
+            writeCellDataxyz $field -time $t
+    
+            grep 500 $t/$field.xyz | sort -g -k 3 > $t/$field.xyzSorted
+            mv $t/$field.xyzSorted $t/$field.xyz
+        done
+    done
+
+    # Graphs of all fields
+    for field in sigma b u massTransfer dPdz; do
+        sed 's/TIME/'$time'/g' plots/$field.gmt > plots/tmp.gmt; \
+        gmtPlot plots/tmp.gmt
+        eps2png $time/$field.eps
+    done
+    montage $time/sigma.png $time/massTransfer.png $time/b.png $time/u.png \
+            $time/dPdz.png -tile 5x1 -geometry +0+0 $time/results.png
+    eog -w $time/results.png &
+    cd ..
+done
+

a47f50e1f04274cc4e118b1cc1864fccae9b0a98
Hilary Weller <h.weller@reading.ac.uk>
2021-06-10 22:58:26 +0100

    Added advection/deformingOnSphere/weakDeformation/MPDATA_latLon_2gauge_adv test cases

---
+++ advection/deformingOnSphere/runAll/plotConvergenceLatLon.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+
+# Assemble l2 error norms as a function of resolution for different schemes
+cases="weakDeformation/MPDATA_latLonRotated_2gauge_adv"
+inputFiles=''
+
+for case in $cases; do
+    echo '#res dx l1error l2error linf mean var min max' > $case/errorNorms.dat
+    inputFiles=(${inputFiles[*]} $case/errorNorms.dat)
+    for res in 60 120 240; do
+        let twoRes=$res*2
+        dir=$case/latLon_${twoRes}x${res}
+        if [[ -a $dir/errorNorms.dat ]]; then
+            dx=`echo $res | awk '{print 180/$1}'`
+            echo -n $res $dx ' ' >> $case/errorNorms.dat
+            tail -1 $dir/errorNorms.dat >> $case/errorNorms.dat
+        fi
+    done
+done
+
+# First and 2nd order lines
+echo '#res dx 1stOrder 2ndOrder' > plots/1st2ndOrder.dat
+echo 360 .5 0.001 0.001 >> plots/1st2ndOrder.dat
+echo 36   5 0.01 0.1 >> plots/1st2ndOrder.dat
+
+inputFiles=(${inputFiles[*]} 
+            plots/1st2ndOrder.dat
+            plots/1st2ndOrder.dat)
+outFile=plots/l2errorCubedWeak.eps
+col=(4 3 4)
+colx=(2 2  2)
+pens=("1p,blue" 
+     "0.5p,black,1_2:0"  "0.5p,black,1_2:0")
+symbols=('x8p' 'x1p' 'x1p')
+legends=("rotated" "1st/2nd" "order")
+
+xlabel='@~D@~x (degrees)'
+#ylabel='l@-2@- error'
+xmin=0.5
+xmax=15
+dx=2
+ddx=2
+dxg=0
+ymin=5e-4
+ymax=5e-2
+dy=2
+ddy=2
+dyg=0
+xscale=*1
+yscale=*1
+legPos=x6/0
+projection=X9cl/4cl
+gv=0
+
+. gmtPlot
+
--- advection/deformingOnSphere/runAll/runSelected.sh
+++ advection/deformingOnSphere/runAll/runSelected.sh
@@ -10,23 +10,31 @@ cases=\
 (\
 #weakDeformation/c8_MPDATA_cubedSphere_2gauge_adv/cube_15
 #weakDeformation/c1p6_MPDATA_cubedSphere_2gauge_adv/cube_15
-weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_15
+#weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_15
 #weakDeformation/c08_MPDATA_cubedSphere_2gauge_adv/cube_15
 
 #weakDeformation/c8_MPDATA_cubedSphere_2gauge_adv/cube_30
 #weakDeformation/c1p6_MPDATA_cubedSphere_2gauge_adv/cube_30
-weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_30
+#weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_30
 #weakDeformation/c08_MPDATA_cubedSphere_2gauge_adv/cube_30
 
 #weakDeformation/c8_MPDATA_cubedSphere_2gauge_adv/cube_60
 #weakDeformation/c1p6_MPDATA_cubedSphere_2gauge_adv/cube_60
-weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_60
+#weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_60
 #weakDeformation/c08_MPDATA_cubedSphere_2gauge_adv/cube_60
 
-weakDeformation/c8_MPDATA_cubedSphere_2gauge_adv/cube_120
-weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_120
-weakDeformation/c1p6_MPDATA_cubedSphere_2gauge_adv/cube_120
-weakDeformation/c08_MPDATA_cubedSphere_2gauge_adv/cube_120
+#weakDeformation/c8_MPDATA_cubedSphere_2gauge_adv/cube_120
+#weakDeformation/c3_MPDATA_cubedSphere_2gauge_adv/cube_120
+#weakDeformation/c1p6_MPDATA_cubedSphere_2gauge_adv/cube_120
+#weakDeformation/c08_MPDATA_cubedSphere_2gauge_adv/cube_120
+
+#weakDeformation/MPDATA_latLonRotated_2gauge_adv/latLon_120x60
+#weakDeformation/MPDATA_latLonRotated_2gauge_adv/latLon_240x120
+#weakDeformation/MPDATA_latLonRotated_2gauge_adv/latLon_480x240
+
+#weakDeformation/MPDATA_latLonRotated_2gauge_adv/latLon_120x60
+weakDeformation/MPDATA_latLon_2gauge_adv/latLon_240x120
+weakDeformation/MPDATA_latLon_2gauge_adv/latLon_480x240
 )
 
 for case in ${cases[*]}; do

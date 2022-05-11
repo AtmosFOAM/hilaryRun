2d7fc7b2add3c161791260ba48932a14a64e2467
Hilary Weller <h.weller@reading.ac.uk>
2022-05-11 07:51:46 +0100

    Minor changes to multiFluidBoussinesqTKEFoam test cases in RayleighBenard/Boussinesq/Ra1e8_Pr1

---
+++ RayleighBenard/Boussinesq/Ra1e8_Pr1/coarseWide_3fluid/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+# clear out old stuff
+rm -rf [0-9]* constant/polyMesh core log
+
+# create mesh
+blockMesh
+
+# Initial conditions
+rm -rf [0-9]* core
+mkdir 0
+cp -r init_0/* 0
+
+# Initial buoyancy gradient
+#setInitialTracerField -name b.air -time 0
+#setFields -time 0
+
+# Solve Boussinesq equations
+multiFluidBoussinesqTKEFoam >& log & sleep 0.01; tail -f log
+

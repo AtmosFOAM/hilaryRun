5114c9080dd10c2d26515a1254d411fe872e1f82
Hilary Weller <h.weller@reading.ac.uk>
2023-04-28 16:56:27 +0100

    Testiing of exnerFoam_uvw in test case vSlice/hillWaves/hydro/coarseHill Currently solution is sensitive to whether gravity waves are implicit or not.

--- vSlice/hillWaves/hydro/coarseHill/run.sh
+++ vSlice/hillWaves/hydro/coarseHill/run.sh
@@ -10,10 +10,11 @@ setTracerField -name muSponge -tracerDict "environmentalProperties"
 
 # Initial conditions
 cp -r init_0 0
+rm 0/P
 setIsothermalBalance
 
 # run
-exnerFoamA > log 2>&1 & sleep 0.01; tail -f log
+exnerFoam_uvw > log 2>&1 & sleep 0.01; tail -f log
 
 #mkdir 0
 #setTracerField -tracerDict expHeightTracerDict -name P
--- vSlice/hillWaves/hydro/plot.sh
+++ vSlice/hillWaves/hydro/plot.sh
@@ -7,3 +7,6 @@ writeuvw -latestTime U
 gmtFoam -latestTime wAll
 ev `ls -1rt */wAll.pdf | tail -1`
 rm [0-9]*/U[x-z]
+
+gmtPlot ../gmtDicts/energy.gmt
+

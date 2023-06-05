2e08e85edfd11fca9515031111ecac610f0e2760
Hilary Weller <h.weller@reading.ac.uk>
2023-06-06 00:27:43 +0100

    Testing exnerFoamRef.     exnerFoamRef solves perturbation equations for u, w. offCentre varies in space.     This is stable for case     RayleighBenard/NSeqns/Agrid/exnerFoamRef     if thetaEqn is NOT solved after final exnerEqn     This is stable for case     vSlice/hillWaves/hydro/     only if thetaEqn IS solved after final exnerEqn.

--- vSlice/hillWaves/hydro/dt100_stdRes/run.sh
+++ vSlice/hillWaves/hydro/dt100_stdRes/run.sh
@@ -15,20 +15,22 @@ cp -r ../init_0 0
 setIsothermalBalance
 mv 0/Exner constant/Exnera
 mv 0/theta constant/thetaa
+cp 0/u 0/w constant
 rm 0/P 0/Uf 0/lnExner 0/muSponge 0/Exnerg
 
 # setup for parallel run
-#decomposePar -constant
+decomposePar -constant
 # run
-#mpirun -np 3 --use-hwthread-cpus 
-exnerFoamRef > log 2>&1 & sleep 0.01; tail -f log
+mpirun -np 3 --use-hwthread-cpus exnerFoamRef -parallel > log 2>&1 &
 
+exit
 
 # Post porcessing
-#reconstructPar; \
-writeuvw -latestTime U; \
+reconstructPar; \
+ writeuvw -latestTime U; \
     gmtFoam  -latestTime w; \
     mkdir -p plots; \
     gmtPlot ../gmtDicts/energy.gmt; \
-    ev 15000/w.pdf plots/energy.eps
+    ev 15000/w.pdf plots/energy.eps; \
+    rm -r processor*/[1-9]*
 
